c1b3a64723986bc9c0038eef008706c84e03014c
Maeva SANCIO <maeva.sancio@mail-esd.com>
2023-01-17 23:56:14 +0100

    3

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+gsap.to(".titleCrew", {
+  motionPath: {
+    path: "#motionPath path",
+    align: "#motionPath path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5],
+  },
+  ease: "none",
+  /*scrollTrigger: {
+    markers: true,
+    trigger: ".titleCrew",
+    start: "top 20%",
+    end: "bottom 50%",
+    scrub: 1,
+  },*/
+});
+
 gsap.to(".element", {
   scale: 1,
   scrollTrigger: {
-    markers: true,
+    //markers: true,
     // l'élément déclencheur
     // en général le même qui est animé à la 1ère ligne
     trigger: ".element",
     // reste du code scroll trigger
     // ..
-    start: "top 0%",
-    end: "bottom 50%",
+    start: "top 80%",
+    end: "bottom 80%",
     scrub: 1,
   },
 });
@@ -19,8 +36,8 @@ gsap.to("#iphone", {
   scrollTrigger: {
     //markers: true,
     trigger: "#iphone",
-    start: "top 30%",
-    end: "bottom 70%",
+    start: "top 80%",
+    end: "bottom 80%",
     scrub: 1,
   },
 });
